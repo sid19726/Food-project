0e037b5fa31ca08fade0c95455f5e7e1a02489a4
sid19726 <sidsidharth219@gmail.com>
2025-04-12 19:20:35 +0530

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+
+
+setTimeout(() => {
+  const loader = document.querySelector(".skeleton-loader");
+  loader.style.display = "none";
+}, 4000);
---
+++ js/menu-offcanvas.js
@@ -0,0 +1,7 @@
+
+document
+    .getElementById("offcanvas-close")
+    .addEventListener("click", function () {
+        var offcanvasSide = document.getElementById("offcanvasNavbar");
+        offcanvasSide.classList.remove("show");
+    });
